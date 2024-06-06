45b9971237e663f3c292f1c8a4702ef575a6eaf1
Сергей Субботин <zarodiny@yandex.ru>
2024-06-06 10:37:01 +1000

    feat: добавляет стили

--- util-functions/reviews.php
+++ util-functions/reviews.php
@@ -8,7 +8,7 @@ function show_reviews()
         )
     );
 
-    $review_box = '<ul class="reviews__list grid sx:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-7">';
+    $review_box = '<ul class="reviews reviews-list justify-items-stretch">';
 
     foreach ($reviews->posts as $review) {
         $review_meta = get_post_meta($review->ID);
@@ -18,41 +18,31 @@ function show_reviews()
             $short_name .= mb_substr($name_element, 0, 1);
         }
 
-        $review_box .= '<li class="relative bg-light-gray rounded-xl">';
-        $review_box .= '<div class="flex justify-between items-center">';
-        $review_box .= '<div class="flex justify-center gap-5 items-center">';
-        $review_box .= '<p class="avatar rounded-full bg-white text-light-green">' . $short_name . '</p>';
-        $review_box .= '<p class="text-black md:text-xl font-medium">' . $review_meta['client_name'][0] . '</p>';
-        $review_box .= '</div>';
+        $review_box .= '<li class="rewiew flex flex-col">';
+        $review_box .= '<div>';
+        $review_box .= '<div class="head flex justify-between mb-7">';
+        $review_box .= '<p class="name">' . $review_meta['client_name'][0] . '</p>';
 
-        $review_box .= '<div class="flex justify-center items-center gap-2">';
-        $review_box .= '<span class="font-medium text-black md:text-xl text-base">' . $review_meta['rating'][0] . '</span>';
-        $review_box .= '<span class="star_symbol"></span>';
-        $review_box .= '</div>';
-        $review_box .= '</div>';
-
-        $review_box .= '<div class="text pt-3">';
-        $review_box .= '<p class="text-black font-medium md:text-lg text-base">' . $review->post_content . '</p>';
-        $review_box .= '</div>';
-
-        $review_box .= '<div class="flex justify-start gap-3 py-3 gallery-wrapper">';
-        $review_box .= '<a href="' . $review_meta['featured_image'][0] . '">';
-        if (isset($review_meta['featured_image'])) {
-
-            $review_box .= '<img class="rounded-lg" src="' . $review_meta['featured_image'][0] . '" alt="">';
-
+        $review_box .= '<div class="flex items-center gap-1">';
+        if (isset($review_meta['star_rating'][0])) {
+            $review_box .= '<div class="grade">' . $review_meta['star_rating'][0] . '</div>';
         }
+        $review_box .= '<img src="' . get_template_directory_uri() . '/src/img/star.svg" width="25"
+                    height="25" alt="star">';
+        $review_box .= '</div>';
+        $review_box .= '</div>';
+        $review_box .= '<div class="comment">' . $review->post_content . '</div>';
+        $review_box .= '</div>';
+
+        $review_box .= '<ul class="gallery flex gap-5">';
+        $review_box .= '<li class="photo">';
+        $review_box .= '<a href="' . $review_meta['featured_image'][0] . '">';
+        $review_box .= '<img src="' . $review_meta['featured_image'][0] . '" height="110" width="110" />';
         $review_box .= '</a>';
-        $review_box .= '</div>';
-
-        $review_box .= '<div class="py-2">';
-        $review_box .= '<span class="text-gray text-sm font-normal">' . date('d.m.Y', strtotime($review->post_date)) . '</span>';
-        $review_box .= '</div>';
         $review_box .= '</li>';
+
+        $review_box .= '</ul>';
     }
-
-    $review_box .= '</ul>';
-
     return $review_box;
 }
 
@@ -65,47 +55,34 @@ function show_reviews_one()
         )
     );
 
-    $review_box = '<ul class="swiper-wrapper justify-items-stretch">';
+    $review_box = '<ul class="reviews swiper-wrapper justify-items-stretch swiper-stretch">';
 
     foreach ($reviews->posts as $review) {
         $review_meta = get_post_meta($review->ID);
-        $reviews_rating = $review_meta['rating'][0];
+        $reviews_rating = $review_meta['star_rating'][0];
 
         $short_name = '';
         foreach (explode(' ', $review_meta['client_name'][0]) as $name_element) {
             $short_name .= mb_substr($name_element, 0, 1);
         }
 
-        $review_box .= '<li class="relative bg-light-gray rounded-xl swiper-slide h-auto p-5">';
-        $review_box .= '<div class="flex justify-between items-center">';
-        $review_box .= '<div class="flex justify-center gap-5 items-center">';
-        $review_box .= '<p class="avatar rounded-full bg-white text-light-green">' . $short_name . '</p>';
-        $review_box .= '<p class="text-black md:text-xl font-medium">' . $review_meta['client_name'][0] . '</p>';
+        $review_box .= '<li class="rewiew swiper-slide flex flex-col">';
+        $review_box .= '<div>';
+        $review_box .= '<div class="head flex justify-between">';
+        $review_box .= '<div class="name">' . $review_meta['client_name'][0] . '</div>';
+        $review_box .= '<div class="grade">' . $review_meta['star_rating'][0] . '</div>';
         $review_box .= '</div>';
 
-        $review_box .= '<div class="flex justify-center items-center gap-2">';
-        $review_box .= '<span class="font-medium text-black md:text-xl text-base">' . $reviews_rating . '</span>';
-        $review_box .= '<span class="star_symbol"></span>';
-        $review_box .= '</div>';
+        $review_box .= '<div class="comment">' . $review->post_content . '</div>';
         $review_box .= '</div>';
 
-        $review_box .= '<div class="text pt-3">';
-        $review_box .= '<p class="text-black font-medium md:text-lg text-base">' . $review->post_content . '</p>';
-        $review_box .= '</div>';
-
-        $review_box .= '<div class="flex justify-start gap-3 py-3 gallery-wrapper">';
+        $review_box .= '<ul class="gallery flex">';
+        $review_box .= '<li class="photo">';
         $review_box .= '<a href="' . $review_meta['featured_image'][0] . '">';
-        if (isset($review_meta['featured_image'])) {
-
-            $review_box .= '<img class="rounded-lg" src="' . $review_meta['featured_image'][0] . '" alt="">';
-
-        }
+        $review_box .= '<img src="' . $review_meta['featured_image'][0] . '" />';
         $review_box .= '</a>';
-        $review_box .= '</div>';
-
-        $review_box .= '<div class="py-2">';
-        $review_box .= '<span class="text-gray text-sm font-normal">' . date('d.m.Y', strtotime($review->post_date)) . '</span>';
-        $review_box .= '</div>';
+        $review_box .= '</li>';
+        $review_box .= '</ul>';
         $review_box .= '</li>';
     }
 
